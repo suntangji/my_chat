f4c5764e219db5423dff20992b6e8193b2b6536d
suntangji <sunxiaoji2012@163.com>
2018-08-12 11:51:13 +0800

    daemon

--- my_chat.cc
+++ my_chat.cc
@@ -5,6 +5,11 @@ int main(int argc, char *argv[]) {
     std::cout << "Usage [addr] [port]" << std::endl;
     return -1;
   }
+  if (daemon(0,0) < 0) {
+    perror("daemon");
+    return -1;
+  };
+
   Server server(argv[1], argv[2]);
   server.Run();
   return 0;
